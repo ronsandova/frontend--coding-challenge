167c4b9aa88efb5890d33f0a7644a0c91f743c5f
daniel sun <danielsun@pickles.com.au>
2022-11-21 11:24:31 +1100

    added password change form in app.js

--- batyr-password-reset/src/App.js
+++ batyr-password-reset/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import PasswordInput from './components/PasswordInput';
 
 function App() {
+  const [formValid, setFormValid] = useState([false, false, false, false])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Change Password</h1>
+      <form>
+        <div>
+          <label>Old Password</label>
+          <PasswordInput required="true"></PasswordInput>
+        </div>
+        <div>New
+          <label> Password</label>
+          <PasswordInput passwordPattern="(?=.*\d)(?=.*\w)(?!.*\s).{8,}"></PasswordInput>
+        </div>
+        <div>
+          <label>Confirm Password</label>
+          <PasswordInput passwordPattern="(?=.*\d)(?=.*\w)(?!.*\s).{8,}"></PasswordInput>
+        </div>
+        <div>
+          <button>Cancel</button>
+          <button>Change Password</button>
+        </div>
+      </form>
     </div>
   );
 }
