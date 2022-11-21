6193231afd2d35fec2cee3597c67433eec84f253
daniel sun <danielsun@pickles.com.au>
2022-11-21 11:07:05 +1100

    added custom error messages to passwordInput

--- batyr-password-reset/src/components/PasswordInput.js
+++ batyr-password-reset/src/components/PasswordInput.js
@@ -1,14 +1,43 @@
+import React, { useState } from "react";
 
 function PasswordInput(props) {
-    const passwordPattern = props?.passwordPattern;
-    const required = props?.required;
+	const [passwordValid, setPasswordValid] = useState({
+		valid: true,
+		errorMsg: "",
+	});
+	const passwordPattern = props?.passwordPattern;
+	const required = props?.required;
 
-     return (
-		<input
-			type="text"
-			pattern={passwordPattern}
-			required={required ? true : false}
-		></input>
+	const onBlurHandler = (event) => {
+		const inputValue = event.target.value;
+
+		if (passwordPattern) {
+			const passwordRegex = new RegExp(passwordPattern);
+			setPasswordValid({
+				valid: passwordRegex.test(inputValue),
+				errorMsg:
+					"Password must be 8 or more characters and a mixture of numbers and letters",
+			});
+		}
+
+		if (required) {
+			setPasswordValid({
+				valid: inputValue > 0,
+				errorMsg: "Password can not be blank",
+			});
+		}
+	};
+
+	return (
+		<div>
+			<input
+				type="text"
+				pattern={passwordPattern}
+				required={required ? true : false}
+				onBlur={onBlurHandler}
+			/>
+			{!passwordValid.valid ? <p>{passwordValid.errorMsg}</p> : null}
+		</div>
 	);
 }
 
